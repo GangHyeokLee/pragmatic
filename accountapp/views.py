677c5b1d018e0aa2ad75e181920147d28600955a
GangHyeok Lee <rkdgur5381@gmail.com>
2023-11-16 21:56:26 +0900

    django course 18 commit: DB access & for loop in HTML & HttpResponseRedirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -7,7 +8,6 @@ from accountapp.models import HelloWorld
 # Create your views here.
 
 def hello_world(request):
-
     if request.method == "POST":
         tmp = request.POST.get('hello_world_input')
 
@@ -15,7 +15,10 @@ def hello_world(request):
         new_hello_world.text = tmp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
-    else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
