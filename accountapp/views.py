71db3855581cb9898f78cc28712f92d2a8589e2d
GangHyeok Lee <rkdgur5381@gmail.com>
2023-11-26 22:43:51 +0900

    django course 45 commit: Project Details

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,7 +10,6 @@ from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 has_ownership = [
@@ -18,26 +17,10 @@ has_ownership = [
 ]
 
 
-@login_required
-def hello_world(request):
-    if request.method == "POST":
-        tmp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = tmp
-        new_hello_world.save()
-
-        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 
@@ -59,7 +42,7 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 
